44f319639a5843e8906f962c1d9011efa88a4f90
Eli Richardson <elijahrich02@icloud.com>
2022-11-19 14:01:47 -0600

    still working on wasm

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"syscall/js"
+
+	"github.com/eli-rich/goc4/src/board"
+)
+
 func main() {
 
+	select {}
+}
+
+func SearchMove() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		fmt.Println("SearchMove")
+		history := args[0].String()
+		fmt.Println(history)
+		b := board.Board{}
+
+		return nil
+	})
 }
